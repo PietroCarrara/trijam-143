2ce383db230fdef3a0339f4aff724567e9c8e493
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2021-10-30 15:30:43 -0300

    Add bad jump

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -3,15 +3,27 @@ extends KinematicBody2D
 onready var sprite: Sprite = $Sprite
 onready var coll: CollisionShape2D = $CollisionShape2D
 
-onready var SPEED = 2*PI*coll.shape.radius
+onready var SPEED = 2*PI*coll.shape.extents.x
+const GRAV = 400
+const JUMP_FORCE = GRAV*(0.25*2) # Takes 0.25 seconds to reach jump peak
+
+var movement = Vector2()
 
 func _physics_process(delta):
-	var movement = Vector2()
 	if Input.is_action_pressed("left"):
-		movement.x -= SPEED
-	if Input.is_action_pressed("right"):
-		movement.x += SPEED
-	movement *= delta
-	move_and_collide(movement)
+		movement.x -= (SPEED*delta)/0.25 # Takes 0.25 seconds to reach max speed
+	elif Input.is_action_pressed("right"):
+		movement.x += (SPEED*delta)/0.25 # Takes 0.25 seconds to reach max speed
+	else:
+		movement.x = 0
+	movement.x = clamp(movement.x, -SPEED, SPEED)
+
+	if is_on_floor():
+		movement.y = 0
+		if Input.is_action_just_pressed("jump"):
+			movement.y -= JUMP_FORCE
+	else:
+		movement.y += GRAV*delta
 	
-	sprite.rotation += sign(movement.x)*2*PI*delta
+	sprite.rotation += (movement.x/SPEED)*2*PI*delta
+	move_and_slide(movement, Vector2.UP)
